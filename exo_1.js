0c787e6e76c3d0aae73149871f4d9cc162942e4b
Théo <55431060+TheoGegout@users.noreply.github.com>
2020-11-23 10:44:13 +0100

    Theo Gégout

--- exo_1.js
+++ exo_1.js
@@ -1,17 +1,24 @@
 let taches = [
-	{
-		nom: 'Travailler efficacement',
-		duree: 180,
-		priorite: 3,
-	},
-	{
-		nom: 'Boire un café',
-		duree: 20,
-		priorite: 1,
-	},
-	{
-		nom: 'Discuter avec ses collèges',
-		duree: 120,
-		priorite: 1,
-	},
+  {
+    nom: "Travailler efficacement",
+    duree: 180,
+    priorite: 3,
+  },
+  {
+    nom: "Boire un café",
+    duree: 20,
+    priorite: 1,
+  },
+  {
+    nom: "Discuter avec ses collèges",
+    duree: 120,
+    priorite: 1,
+  },
 ];
+
+console.log(taches.map((taches) => taches.nom));
+
+console.log(taches.filter((taches) => taches.priorite == 1));
+
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+console.log(taches[taches.duree].reduce(reducer));
