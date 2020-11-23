57a2afc86142e0847bff52826fa6bbabd033fa9e
tofulm <tofulm@gmail.com>
2020-11-23 10:03:26 +0100

    exo 1

---
+++ exo_1.js
@@ -0,0 +1,17 @@
+let taches = [
+	{
+		nom: 'Travailler efficacement',
+		duree: 180,
+		priorite: 3,
+	},
+	{
+		nom: 'Boire un café',
+		duree: 20,
+		priorite: 1,
+	},
+	{
+		nom: 'Discuter avec ses collèges',
+		duree: 120,
+		priorite: 1,
+	},
+];
